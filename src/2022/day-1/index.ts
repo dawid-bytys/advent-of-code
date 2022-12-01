baecff8ebd62c7bc5f1d1a70560f3e501ecc84cd
salvia-dev <salviadev@protonmail.com>
2022-12-01 16:44:56 +0100

    feat: add readInput util

---
+++ src/2022/day-1/index.ts
@@ -0,0 +1,22 @@
+import { readInput } from '../../utils';
+
+const parsedCalories = readInput(2022, 1).split('\n\n');
+
+const totalCaloriesCarriedByEachElf = parsedCalories.map(meal =>
+  meal
+    .split('\n')
+    .map(Number)
+    .reduce((a, b) => a + b),
+);
+
+// *
+const mostCaloriesCarriedByOneElf = Math.max(...totalCaloriesCarriedByEachElf);
+
+// **
+const mostCaloriesCarriedByThreeElves = totalCaloriesCarriedByEachElf
+  .sort((a, b) => b - a)
+  .slice(0, 3)
+  .reduce((a, b) => a + b);
+
+console.log(mostCaloriesCarriedByOneElf); // 67450
+console.log(mostCaloriesCarriedByThreeElves); // 199357
---
+++ src/utils.ts
@@ -0,0 +1,7 @@
+import { readFileSync } from 'fs';
+import { join } from 'path';
+import type { Day, Year } from './types';
+
+export const readInput = (year: Year, day: Day) => {
+  return readFileSync(join(__dirname, `/${year}/day-${day}/input.txt`), 'utf8');
+};
