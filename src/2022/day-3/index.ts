fb7bed58b94b70ab5c5cfbe1065533c5a3a43b99
salvia-dev <salviadev@protonmail.com>
2022-12-03 23:42:46 +0100

    feat(2022): add day-3

---
+++ src/2022/day-3/index.ts
@@ -0,0 +1,53 @@
+import { readInput } from '../../utils';
+
+const input = readInput(2022, 3).split('\n');
+
+const ALPHABET = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+const getFirstCommonChar = (firstString: string, secondString: string, thirdString?: string) => {
+  for (const char of firstString) {
+    if (secondString.includes(char) && (!thirdString || thirdString.includes(char))) {
+      return char;
+    }
+  }
+  return null;
+};
+
+const getHalvesOfString = (input: string) => {
+  const half = Math.floor(input.length / 2);
+  return [input.slice(0, half), input.slice(half)];
+};
+
+// *
+const getSumOfPriorities = (input: string[]) => {
+  let sumOfPriorities = 0;
+
+  for (const line of input) {
+    const [firstCompartment, secondCompartment] = getHalvesOfString(line);
+    const firstCommonChar = getFirstCommonChar(firstCompartment, secondCompartment);
+    if (firstCommonChar) {
+      const indexOfFirstCommonChar = ALPHABET.indexOf(firstCommonChar);
+      sumOfPriorities += indexOfFirstCommonChar + 1;
+    }
+  }
+
+  return sumOfPriorities;
+};
+
+// **
+const getSumOfPrioritiesOfThreeCompartments = (input: string[]) => {
+  let sumOfPriorities = 0;
+
+  for (let i = 0; i < input.length - 2; i += 3) {
+    const firstCommonChar = getFirstCommonChar(input[i], input[i + 1], input[i + 2]);
+    if (firstCommonChar) {
+      const indexOfFirstCommonChar = ALPHABET.indexOf(firstCommonChar);
+      sumOfPriorities += indexOfFirstCommonChar + 1;
+    }
+  }
+
+  return sumOfPriorities;
+};
+
+console.log(getSumOfPriorities(input)); // 7908
+console.log(getSumOfPrioritiesOfThreeCompartments(input)); // 2838
