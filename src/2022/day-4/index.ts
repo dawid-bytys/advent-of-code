f2fa99a7640431d7f142268b3b0500238ca08d28
salvia-dev <salviadev@protonmail.com>
2022-12-04 14:49:07 +0100

    refactor(2022/day-4): functions name

--- src/2022/day-4/index.ts
+++ src/2022/day-4/index.ts
@@ -2,11 +2,11 @@ import { readInput } from '../../utils';
 
 const input = readInput(2022, 4).split('\n');
 
-const doesOverlap = (firstSection: number[], secondSection: number[]) => {
+const doSectionsOverlap = (firstSection: number[], secondSection: number[]) => {
   return firstSection[0] <= secondSection[0] && secondSection[1] <= firstSection[1];
 };
 
-const doesOverlapAtAll = (firstSection: number[], secondSection: number[]) => {
+const doSectionsOverlapAtAll = (firstSection: number[], secondSection: number[]) => {
   return (
     (firstSection[0] <= secondSection[0] && secondSection[0] <= firstSection[1]) ||
     (firstSection[0] <= secondSection[1] && secondSection[1] <= firstSection[1])
@@ -18,11 +18,9 @@ const getCountOfOverlaps = (input: string[]) => {
   let countOfOverlaps = 0;
 
   for (const line of input) {
-    const [firstSection, secondSection] = line
-      .split(',')
-      .map(section => section.split('-').map(Number));
+    const [firstSection, secondSection] = line.split(',').map(section => section.split('-').map(Number));
 
-    if (doesOverlap(firstSection, secondSection) || doesOverlap(secondSection, firstSection)) {
+    if (doSectionsOverlap(firstSection, secondSection) || doSectionsOverlap(secondSection, firstSection)) {
       ++countOfOverlaps;
     }
   }
@@ -35,14 +33,9 @@ const getCountOfOverlapsAtAll = (input: string[]) => {
   let countOfOverlaps = 0;
 
   for (const line of input) {
-    const [firstSection, secondSection] = line
-      .split(',')
-      .map(section => section.split('-').map(Number));
+    const [firstSection, secondSection] = line.split(',').map(section => section.split('-').map(Number));
 
-    if (
-      doesOverlapAtAll(firstSection, secondSection) ||
-      doesOverlapAtAll(secondSection, firstSection)
-    ) {
+    if (doSectionsOverlapAtAll(firstSection, secondSection) || doSectionsOverlapAtAll(secondSection, firstSection)) {
       ++countOfOverlaps;
     }
   }
